8cbec22d3173602bc81446b2b1e5583f5d627122
Dexter Lewis <dexter.lewis@gmail.com>
2020-02-18 20:18:37 -0500

    Able to navigate from room to room

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,9 +39,43 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player_name = input("Hello, what is your name? ")
+player = Player(player_name, room['outside'])
 
 # Write a loop that:
 #
+selection = ''
+print(hasattr(player.location, 'n_to'))
+while selection != 'q':
+    print(f'Hello {player.name}, you are currently in room {player.location}')
+    selection = input('Please enter a direction: ')
+    try:
+        if selection == 'north':
+            if hasattr(player.location, 'n_to'):
+                player.location = player.location.n_to
+            else:
+                print('you\'re not allowed to move there.')
+        elif selection == 'south':
+            if hasattr(player.location, 's_to'):
+                player.location = player.location.s_to
+            else:
+                print('you\'re not allowed to move there.')
+        elif selection == 'west':
+            if hasattr(player.location, 'w_to'):
+                player.location = player.location.w_to
+            else:
+                print('you\'re not allowed to move there.')
+        elif selection == 'east':
+            if hasattr(player.location, 'e_to'):
+                player.location = player.location.e_to
+            else:
+                print('you\'re not allowed to move there.')
+        else:
+            print('You cannot travel there')
+    except ValueError:
+        print('Error, unrecognized input')
+
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
